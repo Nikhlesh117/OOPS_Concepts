7810efd6c737b73822ee82c5ec97fbf1373c8871
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-22 08:59:56 +0530

    Added: Class and Object

---
+++ OOPS_Concept/OOPS_Concept/ClassName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concept
+{
+    public class ClassName
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("Heello");
+        }
+    }
+}
+
---
+++ OOPS_Concept/OOPS_Concept/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPS_Concept
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClassName name = new ClassName();
+            name.MethodOne();
+            Console.ReadKey();
+
+        }
+    }
+}
