fe2cd5682aba28e5079791ca00850c9733fa5c4b
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-27 18:15:45 +0530

    Added: AccessModifiers

---
+++ OOPS_Concept/AccessModifiers/AccessModifiers/Modifiers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    //AccessModifiers in C# are the keywords used to Specify the accessability or scope of Variable of function
+    public class Modifiers
+    {
+        // public access modifier can be access in same asseblt or in different assembley
+        public int a = 10;
+        //private we cant access anywhere outside the class it access only within current class
+        private void PrivateMethod()
+        {
+            Console.WriteLine("PrivateMethod");
+        }
+        // It allow code in same class or derive class
+        protected string str = "hello";
+        // The access is limited to the current class or
+        protected internal double val = 2.4;
+    }
+}
---
+++ OOPS_Concept/AccessModifiers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace AccessModifiers
+{
+    public class Program: Modifiers
+    {
+        public static void Main(string[] args)
+        {
+            Modifiers obj= new Modifiers();
+            //public
+            Console.WriteLine(obj.a);
+            //private
+            //obj.PrivateMethod();
+            //protected
+            Program program= new Program();
+            Console.WriteLine(program.str);
+            //protected internal
+            Console.WriteLine(program.val);
+        }
+    }
+}
